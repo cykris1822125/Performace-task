4085d20ac77f15c14cce9a8fffe42a1f5d444993
cykris1822125 <76935565+cykris1822125@users.noreply.github.com>
2021-02-26 14:20:11 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head> <TITLE> TLE PT </TITLE>
+</head>
+<framset cols= "50%, 50%>
+<frameset src= "">
+<frameset src= "https://sleepy-springs-75293.herokuapp.com/">
+<frameset rows= "50%,  50%">
+<frame src= "https://youtube.com/watch?v=i0ZabxXmH4Y&feature=share">
+<frame src= "https://youtu.be/XCzzf4KGiM0">
+</frameset>
+</html>
