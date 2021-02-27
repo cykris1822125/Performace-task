17b58d67b7fc9b7331011b244444bfe35d393d95
cykris1822125 <76935565+cykris1822125@users.noreply.github.com>
2021-02-28 01:19:20 +0800

    Update and rename README.md to Index.php

---
+++ Index.php
@@ -0,0 +1,32 @@
+<html>
+<head> <TITLE> TLE PT </TITLE>
+</head>
+<body>
+<body bgcolor = "#faace5">
+
+<center><h1><font size = 8> How covid 19 Affect us </h1> </center></font size><br><br>
+
+<center><img src = "1.jpg" width = "400px" Hieght = "500px">
+  	<img src = "2.jpg" width = "400px" Hieght = "500px"> </center> <br><br>
+
+
+
+<center> <font size = "5"><b>Many people loss their job, main income, affect businesses and even 
+suffer on their own finances. Affects people's lives physically, mentally and spiritually as 
+well. When we take a look at on it's bright side we get to know ourself here, with a span of 
+time we focus on how to grow our self confidence, and even let us realize a lot of things like 
+how to handle our finances. And reflect to our own doings. Covid-19 has advantage and 
+disadvantages in some ways, is that how people deal and react to it.<br><br>
+
+Nowadays, many people are still suffering because of Covid 19. And still the virus still there 
+and many people loss their job, main income, affect businesses and the income that they spend 
+towards their Family is not suitable anymore and all they need to do is to save the money and 
+spend it in buying groceries that are needed. Both Positive and negative affects the Covid 19 to 
+us the positive is our surroundings is sanitize clean and they following the protocols like 
+wearing mask and always bringing alcohol. The negative is that many people are struggling on how 
+to communicate with their business beacuse we all know that we meet through zoom and sometimes 
+we didn't know when the signal is okay and not okay. So still we are waiting for the good 
+updates and praying to back to normal.</center></font size></b>
+
+</body>
+</html>
--- README.md
+++
@@ -1,1 +0,0 @@
-# Performace-task
